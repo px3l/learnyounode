3c07f4bb79e5efb36e81fe5e2704a9a3391b1d0d
px3l <connor@jenca.io>
2016-04-16 13:29:00 +0100

    juggling async answer

--- jugglingasync.js
+++ jugglingasync.js
@@ -1,15 +1,41 @@
 var http = require("http");
 var bl = require("bl");
 
-var callbackCounter = 0;
+// pushing urls to an array
+
+var urls = []
 
 for (var i=2; i<process.argv.length; i++) {
-	http.get(process.argv[i], function(response){
-		response.pipe(bl(function (err, data){
-			callbackCounter += 1
-			if (callbackCounter == 3) {
-				console.log(data.toString())
-			}
-		}))
-	})
+	var url = process.argv[i]
+	urls.push(url)
 }
+
+// array of functions to run with each value
+
+var handlers = urls.map(function(url){
+	return function(callback){
+		http.get(url, function(response){
+			response.pipe(bl(function (err, data){
+				if (err) return callback(err)
+				callback(null, data.toString())
+			}))
+		})
+	}
+})
+
+// store the url responses
+
+var storage = []
+var counter = 0
+
+handlers.forEach(function(fn, i){
+	fn(function(err, data){
+		storage[i] = data
+		counter++
+		if (counter >= urls.length){
+			storage.forEach(function(st){
+				console.log(st)
+			})
+		}
+	})
+})
