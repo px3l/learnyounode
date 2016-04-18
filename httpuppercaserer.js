2eaf063642b386dbf85c953e282cffa137ef8afb
px3l <connor@jenca.io>
2016-04-18 16:00:03 +0100

    http uppercaserer not uppercasing

--- httpuppercaserer.js
+++ httpuppercaserer.js
@@ -5,9 +5,11 @@ var map = require("through2-map");
 var port = process.argv[2];
 
 var httpServer = http.createServer(function(req, res){
-	//receives POST requests only and converts to uppercase then returns to client
-	var inStream = fs.createReadStream(req)
-	inStream.pipe(map(function(req){
-		return req.toString().toUpperCase()
+
+	req.pipe(map(function(req){
+		return req.toString().toUpperCase();
 	}).pipe(res))
+
 })
+
+httpServer.listen(port)
