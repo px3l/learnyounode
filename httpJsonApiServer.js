d20bc7e2b64b6570ac5d8a6b5496dcf3f452e9cf
px3l <connor@jenca.io>
2016-04-20 12:39:56 +0100

    final two challenges broken

--- httpJsonApiServer.js
+++ httpJsonApiServer.js
@@ -2,15 +2,26 @@ var http = require("http");
 var url = require("url");
 var port = process.argv[2];
 
+function makeTime(time){
+	return {
+		hour: time.getHours(),
+		minute: time.getMinutes(),
+		second: time.getSeconds()
+	}
+}
+
+function unixTime(time){
+	return {
+		unixtime: time.getTime()
+	}
+
 var httpServer = http.createServer(function(req, res){
-	var path = url.parse(req.url, true)
+	var path = url.parse(req.url, true);
+	var time = new Date(path.query.iso);
+	var result;
+
 	if (path == "/api/parsetime"){
 		
-		res = 	{
-					"hour": 14,
-					"minute": 23,
-					"second": 15
-				}
 	}
 })
 
--- httpUppercaserer.js
+++ httpUppercaserer.js
@@ -1,6 +1,5 @@
 var http = require("http");
 var map = require("through2-map");
-
 var port = process.argv[2];
 
 var httpServer = http.createServer(function(req, res){
@@ -8,7 +7,7 @@ var httpServer = http.createServer(function(req, res){
 	req.pipe(map(function(chunk){
 		return chunk.toString().toUpperCase();
 	}).pipe(res))
-
+	
 })
 
 httpServer.listen(port)
